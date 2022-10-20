af46cd869d7313a9febe759ac0e76c19b06cb84f
Eddy Filip <eddy.filip@agilebits.com>
2022-10-20 13:05:56 +0200

    Simplify test suite to reduce code duplication

--- pkg/webhook/webhook_test.go
+++ pkg/webhook/webhook_test.go
@@ -35,6 +35,77 @@ func parseResponseBody(rr *httptest.ResponseRecorder) *admissionv1.AdmissionResp
 	return resBody.Response
 }
 
+var testNotPatch = map[string]struct {
+	pod corev1.Pod
+}{
+	"Pod has no inject annotation": {
+		pod: corev1.Pod{},
+	},
+	"Inject annotation is empty": {
+		pod: corev1.Pod{
+			ObjectMeta: metav1.ObjectMeta{
+				Annotations: map[string]string{
+					"operator.1password.io/inject": "",
+				},
+			},
+		},
+	},
+	"Inject status annotation is 'injected'": {
+		pod: corev1.Pod{
+			ObjectMeta: metav1.ObjectMeta{
+				Annotations: map[string]string{
+					"operator.1password.io/status": "injected",
+				},
+			},
+		},
+	},
+	"Inject label does not have a match with container name": {
+		pod: corev1.Pod{
+			ObjectMeta: metav1.ObjectMeta{
+				Annotations: map[string]string{
+					"operator.1password.io/inject": "not-existing-container",
+				},
+			},
+			Spec: corev1.PodSpec{
+				InitContainers: []corev1.Container{
+					{Name: "app-init"},
+				},
+				Containers: []corev1.Container{
+					{Name: "app"},
+				},
+			},
+		},
+	},
+	"Container does not have a command provided": {
+		pod: corev1.Pod{
+			ObjectMeta: metav1.ObjectMeta{
+				Annotations: map[string]string{
+					"operator.1password.io/inject": "app",
+				},
+			},
+			Spec: corev1.PodSpec{
+				Containers: []corev1.Container{
+					{Name: "app"},
+				},
+			},
+		},
+	},
+	"Init-container does not have a command provided": {
+		pod: corev1.Pod{
+			ObjectMeta: metav1.ObjectMeta{
+				Annotations: map[string]string{
+					"operator.1password.io/inject": "app",
+				},
+			},
+			Spec: corev1.PodSpec{
+				InitContainers: []corev1.Container{
+					{Name: "app"},
+				},
+			},
+		},
+	},
+}
+
 var _ = Describe("Webhook Test", Ordered, func() {
 	var rr *httptest.ResponseRecorder
 	var handler http.HandlerFunc
@@ -53,14 +124,14 @@ var _ = Describe("Webhook Test", Ordered, func() {
 		rr = nil
 	})
 
-	It("should return bad request if no body provided", func() {
+	It("Should return bad request if no body provided", func() {
 		req := createRequest(nil)
 		handler.ServeHTTP(rr, req)
 
 		Expect(rr.Code).To(Equal(http.StatusBadRequest))
 	})
 
-	It("should return StatusUnsupportedMediaType if Content-Type is not application/json", func() {
+	It("Should return StatusUnsupportedMediaType if Content-Type is not application/json", func() {
 		req := createRequest(strings.NewReader("some content"))
 		req.Header.Del("Content-Type")
 		handler.ServeHTTP(rr, req)
@@ -68,7 +139,7 @@ var _ = Describe("Webhook Test", Ordered, func() {
 		Expect(rr.Code).To(Equal(http.StatusUnsupportedMediaType))
 	})
 
-	It("should return error if no pod provided", func() {
+	It("Should return error if no pod provided", func() {
 		ar := admissionv1.AdmissionReview{
 			Request: &admissionv1.AdmissionRequest{
 				Namespace: "default",
@@ -87,213 +158,31 @@ var _ = Describe("Webhook Test", Ordered, func() {
 	})
 
 	Context("NOT create a patch", func() {
-		When("pod has no inject annotation", func() {
-			It("should NOT create patch", func() {
-				pod := corev1.Pod{}
-				raw, err := json.Marshal(pod)
-				Expect(err).NotTo(HaveOccurred())
+		for testCase, testData := range testNotPatch {
+			When(testCase, func() {
+				It("Should NOT create patch", func() {
+					raw, err := json.Marshal(testData.pod)
+					Expect(err).NotTo(HaveOccurred())
 
-				ar := admissionv1.AdmissionReview{
-					Request: &admissionv1.AdmissionRequest{
-						Namespace: "default",
-						Object: runtime.RawExtension{
-							Raw: raw,
+					ar := admissionv1.AdmissionReview{
+						Request: &admissionv1.AdmissionRequest{
+							Namespace: "default",
+							Object: runtime.RawExtension{
+								Raw: raw,
+							},
 						},
-					},
-				}
-				body, err := json.Marshal(ar)
-				Expect(err).NotTo(HaveOccurred())
+					}
+					body, err := json.Marshal(ar)
+					Expect(err).NotTo(HaveOccurred())
 
-				req := createRequest(bytes.NewReader(body))
-				handler.ServeHTTP(rr, req)
+					req := createRequest(bytes.NewReader(body))
+					handler.ServeHTTP(rr, req)
 
-				responseBody := parseResponseBody(rr)
-				Expect(responseBody.Patch).To(BeNil())
+					responseBody := parseResponseBody(rr)
+					Expect(responseBody.Patch).To(BeNil())
+				})
 			})
-		})
-
-		When("inject annotation is empty", func() {
-			It("should NOT create patch", func() {
-				pod := corev1.Pod{
-					ObjectMeta: metav1.ObjectMeta{
-						Annotations: map[string]string{
-							"operator.1password.io/inject": "",
-						},
-					},
-				}
-				raw, err := json.Marshal(pod)
-				Expect(err).NotTo(HaveOccurred())
-
-				ar := admissionv1.AdmissionReview{
-					Request: &admissionv1.AdmissionRequest{
-						Namespace: "default",
-						Object: runtime.RawExtension{
-							Raw: raw,
-						},
-					},
-				}
-				body, err := json.Marshal(ar)
-				Expect(err).NotTo(HaveOccurred())
-
-				req := createRequest(bytes.NewReader(body))
-				handler.ServeHTTP(rr, req)
-
-				Expect(rr.Code).To(Equal(http.StatusOK))
-
-				responseBody := parseResponseBody(rr)
-				Expect(responseBody.Patch).To(BeNil())
-			})
-		})
-
-		When("inject status annotation is 'injected'", func() {
-			It("should NOT create patch", func() {
-				pod := corev1.Pod{
-					ObjectMeta: metav1.ObjectMeta{
-						Annotations: map[string]string{
-							"operator.1password.io/status": "injected",
-						},
-					},
-				}
-				raw, err := json.Marshal(pod)
-				Expect(err).NotTo(HaveOccurred())
-
-				ar := admissionv1.AdmissionReview{
-					Request: &admissionv1.AdmissionRequest{
-						Namespace: "default",
-						Object: runtime.RawExtension{
-							Raw: raw,
-						},
-					},
-				}
-				body, err := json.Marshal(ar)
-				Expect(err).NotTo(HaveOccurred())
-
-				req := createRequest(bytes.NewReader(body))
-				handler.ServeHTTP(rr, req)
-
-				Expect(rr.Code).To(Equal(http.StatusOK))
-
-				responseBody := parseResponseBody(rr)
-				Expect(responseBody.Patch).To(BeNil())
-			})
-		})
-
-		When("inject label does not have a match with container name", func() {
-			It("should NOT create patch", func() {
-				pod := corev1.Pod{
-					ObjectMeta: metav1.ObjectMeta{
-						Annotations: map[string]string{
-							"operator.1password.io/inject": "not-existing-container",
-						},
-					},
-					Spec: corev1.PodSpec{
-						InitContainers: []corev1.Container{
-							{Name: "app-init"},
-						},
-						Containers: []corev1.Container{
-							{Name: "app"},
-						},
-					},
-				}
-				raw, err := json.Marshal(pod)
-				Expect(err).NotTo(HaveOccurred())
-
-				ar := admissionv1.AdmissionReview{
-					Request: &admissionv1.AdmissionRequest{
-						Namespace: "default",
-						Object: runtime.RawExtension{
-							Raw: raw,
-						},
-					},
-				}
-				body, err := json.Marshal(ar)
-				Expect(err).NotTo(HaveOccurred())
-
-				req := createRequest(bytes.NewReader(body))
-				handler.ServeHTTP(rr, req)
-
-				Expect(rr.Code).To(Equal(http.StatusOK))
-
-				responseBody := parseResponseBody(rr)
-				Expect(responseBody.Patch).To(BeNil())
-			})
-		})
-
-		When("container does not have a command provided", func() {
-			It("should NOT create patch", func() {
-				pod := corev1.Pod{
-					ObjectMeta: metav1.ObjectMeta{
-						Annotations: map[string]string{
-							"operator.1password.io/inject": "app",
-						},
-					},
-					Spec: corev1.PodSpec{
-						Containers: []corev1.Container{
-							{Name: "app"},
-						},
-					},
-				}
-				raw, err := json.Marshal(pod)
-				Expect(err).NotTo(HaveOccurred())
-
-				ar := admissionv1.AdmissionReview{
-					Request: &admissionv1.AdmissionRequest{
-						Namespace: "default",
-						Object: runtime.RawExtension{
-							Raw: raw,
-						},
-					},
-				}
-				body, err := json.Marshal(ar)
-				Expect(err).NotTo(HaveOccurred())
-
-				req := createRequest(bytes.NewReader(body))
-				handler.ServeHTTP(rr, req)
-
-				Expect(rr.Code).To(Equal(http.StatusOK))
-
-				responseBody := parseResponseBody(rr)
-				Expect(responseBody.Patch).To(BeNil())
-			})
-		})
-
-		When("init-container does not have a command provided", func() {
-			It("should NOT create patch", func() {
-				pod := corev1.Pod{
-					ObjectMeta: metav1.ObjectMeta{
-						Annotations: map[string]string{
-							"operator.1password.io/inject": "app",
-						},
-					},
-					Spec: corev1.PodSpec{
-						InitContainers: []corev1.Container{
-							{Name: "app"},
-						},
-					},
-				}
-				raw, err := json.Marshal(pod)
-				Expect(err).NotTo(HaveOccurred())
-
-				ar := admissionv1.AdmissionReview{
-					Request: &admissionv1.AdmissionRequest{
-						Namespace: "default",
-						Object: runtime.RawExtension{
-							Raw: raw,
-						},
-					},
-				}
-				body, err := json.Marshal(ar)
-				Expect(err).NotTo(HaveOccurred())
-
-				req := createRequest(bytes.NewReader(body))
-				handler.ServeHTTP(rr, req)
-
-				Expect(rr.Code).To(Equal(http.StatusOK))
-
-				responseBody := parseResponseBody(rr)
-				Expect(responseBody.Patch).To(BeNil())
-			})
-		})
+		}
 	})
 
 	// TODO: cover cases when patch is created, check it contains env vars, volumes etc.
